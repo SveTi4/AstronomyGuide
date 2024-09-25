d3e814db72dd9821c7d48e312c72170871d50c67
SveTi4 <studymail2.7.53@gmail.com>
2024-09-25 18:51:20 +0700

    Fix commentaries

--- app/src/main/java/com/example/astronomyguide/opengl/MyGLRenderer.kt
+++ app/src/main/java/com/example/astronomyguide/opengl/MyGLRenderer.kt
@@ -16,11 +16,11 @@ class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
     private val viewMatrix = FloatArray(16)
 
     override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
-        // Set the background clear color to black
+        // Устанавливаем цвет очистки экрана (чёрный)
         GLES20.glClearColor(0f, 0f, 0f, 1f)
-        // Enable depth testing
+        // Включаем тест глубины
         GLES20.glEnable(GLES20.GL_DEPTH_TEST)
-        // Initialize the square
+        // Инициализируем квадрат
         square = Square(context)
     }
 
@@ -28,25 +28,26 @@ class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
         // Set the OpenGL viewport to the full surface
         GLES20.glViewport(0, 0, width, height)
         val ratio: Float = width.toFloat() / height.toFloat()
-        // Set up the projection matrix
+        // Устанавливаем перспективную проекцию
         Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 1f, 10f)
     }
 
     override fun onDrawFrame(gl: GL10) {
-        // Clear the screen
+        // Очищаем экран
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
 
-        // Set up the view matrix
+        // Устанавливаем камеру
         Matrix.setLookAtM(
             viewMatrix, 0,
-            0f, 0f, 3f,   // Camera position
-            0f, 0f, 0f,   // Look at point
-            0f, 1f, 0f    // Up vector
+            0f, 0f, 3f,   // Положение камеры
+            0f, 0f, 0f,   // Точка, на которую смотрим
+            0f, 1f, 0f    // Вектор "вверх"
         )
 
+        // Масштабируем квадрат, чтобы он заполнил фон
         Matrix.scaleM(viewMatrix, 0, 2f, 3f, 1f)
 
-        // Calculate the MVP matrix
+        // Вычисляем итоговую матрицу MVP для квадрата
         Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
         // Draw the square
